1d64b212aabfb5228d921ba6146c218167807ef2
Diogo Thou <diogo.s.leite@hotmail.com>
2025-07-09 03:36:01 -0300

    primeiro commit do CNPJ Enriche1111111111111111111111aaz1daz1zr

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 const express = require('express');
 const axios = require('axios');
 const syncCNPJs = require('./syncCNPJs');
