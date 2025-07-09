94ad2d328bb7da42513927a87416a142ec45703e
Diogo Thou <diogo.s.leite@hotmail.com>
2025-07-08 22:06:07 -0300

    fix: add GET support to /api/sync-cnp1j

--- pages/api/sync-cnpj.js
+++ pages/api/sync-cnpj.js
@@ -2,9 +2,8 @@
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    return res.status(200).json({ message: 'GET recebido com sucesso!' });
+    return res.status(200).json({ message: 'GET funcionando!' });
   }
 
-  res.setHeader('Allow', ['GET']);
-  return res.status(405).end(`Método ${req.method} não permitido`);
+  res.status(405).json({ message: 'Método não permitido' });
 }
