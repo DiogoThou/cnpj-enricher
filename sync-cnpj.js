0396320a9aac0fda178315e1214488e88afd5450
Diogo Thou <diogo.s.leite@hotmail.com>
2025-07-08 22:49:12 -0300

    primeiro commit do CNPJ Enriche1111r

--- sync-cnpj.js
+++
@@ -1,9 +0,0 @@
-// pages/api/sync-cnpj.js
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    return res.status(200).json({ message: 'GET funcionando!' });
-  }
-
-  res.status(405).json({ message: 'Método não permitido' });
-}
